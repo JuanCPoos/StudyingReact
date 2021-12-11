d72191cc808c3aa7763fbeac063edbf04c4c49c7
JuanCPoos <juan.cpoos@gmail.com>
2021-12-11 10:38:30 -0300

    change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HolaMundo(nombre){
+  var presentacion = <h2>Hola, soy {nombre}</h2>;
+
+  return presentacion;
+}
+
 function App() {
+  var nombre = "Juan";
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>Welcome to course React 
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {HolaMundo(nombre)}
         <a
           className="App-link"
           href="https://reactjs.org"
